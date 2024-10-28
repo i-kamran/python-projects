93edbd4337360cacc694fae0b1b6d791a6cb763b
i-kamran <ibragimovkmrn@gmail.com>
2024-10-28 21:14:57 +0500

    feat(task-tracker): Util func to save to JSON

--- task_tracker/task_tracker/utils.py
+++ task_tracker/task_tracker/utils.py
@@ -32,3 +32,32 @@ def load_tasks(path: str) -> List[Dict[str, Union[str, bool]]]:
 
     with open(tasks_file, "r", encoding="UTF-8") as file:
         return json.load(file)
+
+
+def write_tasks_to_json(task_list: List[Task], path: str) -> None:
+    """
+    Write the in-memory task list to a JSON file.
+
+    Parameters
+    ----------
+    task_list : List[Task]
+        The list of tasks to be written into the JSON file.
+    path : str
+        Path to the JSON file where tasks will be saved.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    RuntimeError
+        If writing to the JSON file fails.
+    """
+    task_file = Path(path)
+    try:
+        with open(task_file, "w", encoding="UTF-8") as file:
+            json.dump(task_list, file, indent=2)
+    except Exception as e:
+        raise RuntimeError(f"Failed to write tasks to {path}") from e
+
