afe498254b5af89bba20238095e009adc0a26ccb
i-kamran <ibragimovkmrn@gmail.com>
2024-11-01 17:12:56 +0500

    feat(t-t): Add main add task function

--- task_tracker/task_tracker/main.py
+++ task_tracker/task_tracker/main.py
@@ -1,6 +1,27 @@
+import argparse
+
+from task_tracker import utils
+
+PATH = "./tasks.json"
+
+
 def main():
     pass
 
 
-if __name__ == "__main__":
-    main()
+def add(desc: str) -> None:
+    """
+    Add new task to the list.
+
+    Parameters
+    ----------
+    desc: str
+        Description of the task
+
+    Returns
+    -------
+    None
+    """
+
+    tasks = utils.add_task(desc, utils.load_tasks(PATH))
+    utils.write_tasks_to_json(tasks, PATH)
